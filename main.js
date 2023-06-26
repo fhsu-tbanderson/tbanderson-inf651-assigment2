efe982d416571658d51d73cc74f1e42e88d58f14
fhsu-tbanderson <tbanderson@mail.fhsu.edu>
2023-06-25 20:14:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+let myNumber;
+myNumber = Number(myNumber);
+myNumber = 5;
+
+let myOtherNumber;
+myOtherNumber = Number(myOtherNumber);
+myOtherNumber = 5;
+
+let myTotal;
+myTotal = Number(myTotal);
+myTotal = myNumber * myOtherNumber;
+
+
+let myName;
+myName = String(myName);
+
+let myCombo;
+myCombo = String(myCombo);
+myCombo = myNumber + myOtherNumber + "";
+
+let myOtherCombo;
+myOtherCombo = Number(myOtherCombo);
+myOtherCombo = myNumber + myOtherNumber;
+
+
+let myRemainder;
+myRemainder = Number(myRemainder);
+myRemainder = myNumber % 5;
+
+let i;
+i = Number(i);
+i = 2;
+
+let int;
+int = Number(int);
+int = 4;
+
+let myBoolean;
+myBoolean = Boolean(myBoolean);
+myBoolean = false;
